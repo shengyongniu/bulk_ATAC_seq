a20d5c4c038ec343541c7d8d67e1d76a060a94bb
NIU, SHENG-YONG <niu.shengyong@gmail.com>
2018-09-10 14:18:05 -0700

    Update peakCalling_y.sh

--- peakCalling_y.sh
+++ peakCalling_y.sh
@@ -10,7 +10,7 @@ echo "NCORE_REQ=${NCORE_REQ}"
 RT_LIMIT="48:00:00"
 MEM_LIMIT="125G" # it seems each job do not consume 10G to my observation; so set 125G shall be safe, even after increase the cores. 
  
-FILE_COUNT=`find /restricted/projectnb/chengcancer/Simon/project/T2D/ATAC-seq/NextSeq/4shift/*.bam | wc -l`
+FILE_COUNT=`find /restricted/projectnb/*.bam | wc -l`
 echo "FILE_COUNT=${FILE_COUNT}"
 
 qsub -P $PROJECT -N $JOBNAME -pe omp $NCORE_REQ -l mem_total=${MEM_LIMIT} -l h_rt=${RT_LIMIT} -t 1-${FILE_COUNT} peakCalling_y.qsub
