f164a31a1219fce75eb68bb1d5cd8095cb3ced36
NIU, SHENG-YONG <niu.shengyong@gmail.com>
2018-09-14 19:36:42 -0700

    Update peakCalling_y.sh

--- peakCalling_y.sh
+++ peakCalling_y.sh
@@ -1,16 +1,7 @@
 #!/bin/bash
 
-PROJECT=chengcancer
-echo "PROJECT=${PROJECT}"
-#JOBNAME=`(basename $0) |  sed -e 's/\.[^.]*$//'` #tophat_mapping_y
-JOBNAME='peakCalling_batch5'
-echo "JOBNAME=${JOBNAME}"
-NCORE_REQ=1 #based on Charlie's benmark, the increased number of cores above 4 didn't play much in performance gain for tophat,user can experiment this more to set the best value
-echo "NCORE_REQ=${NCORE_REQ}"
-RT_LIMIT="48:00:00"
-MEM_LIMIT="125G" # it seems each job do not consume 10G to my observation; so set 125G shall be safe, even after increase the cores. 
- 
-FILE_COUNT=`find /restricted/projectnb/*.bam | wc -l`
+
+FILE_COUNT=`find ./*.bam | wc -l`
 echo "FILE_COUNT=${FILE_COUNT}"
 
 qsub -P $PROJECT -N $JOBNAME -pe omp $NCORE_REQ -l mem_total=${MEM_LIMIT} -l h_rt=${RT_LIMIT} -t 1-${FILE_COUNT} peakCalling_y.qsub
