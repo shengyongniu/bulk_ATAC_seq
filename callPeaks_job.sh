9a8a26cfae220d671dcd462ee91d03af7bd2903a
NIU, SHENG-YONG <niu.shengyong@gmail.com>
2018-09-14 19:31:49 -0700

    Update callPeaks_job.sh

--- callPeaks_job.sh
+++ callPeaks_job.sh
@@ -6,12 +6,12 @@ module load python/2.7.7
 module load macs/2.1.0.20150731
 
 
-for filename in /restricted/projectnb/chengcancer/Simon/project/T2D/ATAC-seq/NextSeq/4shift/*.bam ; do
+for filename in ./*.bam ; do
 
 
 
 
-macs2 callpeak -t "$filename"  -f BED -g hs -nomodel --nolambda --keep-dup all --call-summits --outdir /restricted/projectnb/chengcancer/Simon/project/T2D/ATAC-seq/NextSeq/8callPeaknew/  -n $(basename "$filename" .bed) -B -q 0.01 --bdg  --format BAMPE
+macs2 callpeak -t "$filename"  -f BED -g hs -nomodel --nolambda --keep-dup all --call-summits --outdir .  -n $(basename "$filename" .bed) -B -q 0.01 --bdg  --format BAMPE
 
 
 
