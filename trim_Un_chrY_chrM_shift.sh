c3887285d8f0ac3b44de76f0743918eb0c7ddcb0
NIU, SHENG-YONG <niu.shengyong@gmail.com>
2018-09-14 19:39:45 -0700

    Update trim_Un_chrY_chrM_shift.sh

--- trim_Un_chrY_chrM_shift.sh
+++ trim_Un_chrY_chrM_shift.sh
@@ -1,17 +1,6 @@
 #!/bin/bash
 
-
-PROJECT=chengcancer
-echo "PROJECT=${PROJECT}"
-#JOBNAME=`(basename $0) |  sed -e 's/\.[^.]*$//'` #tophat_mapping_y
-JOBNAME='trim_shift_bai'
-echo "JOBNAME=${JOBNAME}"
-NCORE_REQ=1 #based on Charlie's benmark, the increased number of cores above 4 didn't play much in performance gain for tophat,user can experiment this more to set the best value
-echo "NCORE_REQ=${NCORE_REQ}"
-RT_LIMIT="48:00:00"
-MEM_LIMIT="125G" # it seems each job do not consume 10G to my observation; so set 125G shall be safe, even after increase the cores.
-
-FILE_COUNT=`find /restricted/projectnb/chengcancer/Simon/project/T2D/ATAC-seq/NextSeq/3readsFilter/*.sorted.dedup.bam | wc -l`
+FILE_COUNT=`find ./*.sorted.dedup.bam | wc -l`
 echo "FILE_COUNT=${FILE_COUNT}"
 
 qsub -P $PROJECT -N $JOBNAME -pe omp $NCORE_REQ -l mem_total=${MEM_LIMIT} -l h_rt=${RT_LIMIT} -t 1-${FILE_COUNT} trim_Un_chrY_chrM_shift.qsub
