dca042b1dc74f0cae8d199e64e3251c36785a3f8
NIU, SHENG-YONG <niu.shengyong@gmail.com>
2018-09-14 19:30:52 -0700

    Update bedtobam.sh

--- bedtobam.sh
+++ bedtobam.sh
@@ -2,10 +2,10 @@
 
 module load bedtools/2.24.0
 
-for filename in /restricted/projectnb/chengcancer/Simon/project/T2D/ATAC-seq/NextSeq/4shift/*nonnegative.bed ; do
+for filename in ./*nonnegative.bed ; do
 
 
-bedtools bedtobam -i "$filename"    -g /restricted/projectnb/chengcancer/Simon/project/T2D/ATAC-seq/NextSeq/4shift/hg38.genome >  /restricted/projectnb/chengcancer/Simon/project/T2D/ATAC-seq/NextSeq/4shift/$(basename "$filename" .bed).bam
+bedtools bedtobam -i "$filename"    -g ./hg38.genome >  ./$(basename "$filename" .bed).bam
 
 
 
