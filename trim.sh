d0048dbc94800ef2a6c2c3d595e4e91e653f6185
NIU, SHENG-YONG <niu.shengyong@gmail.com>
2018-09-14 19:38:57 -0700

    Update trim.sh

--- trim.sh
+++ trim.sh
@@ -2,11 +2,11 @@
 
 module load trimmomatic/0.33
 
-for filename in /restricted/projectnb/chengcancer/Simon/project/T2D/ATAC-seq/NextSeq/0raw/merge/*_R1_001.fastq.gz 
+for filename in ./*_R1_001.fastq.gz 
 
 do
 
-java -jar $SCC_TRIMMOMATIC_BIN/trimmomatic.jar PE  "$filename"  /restricted/projectnb/chengcancer/Simon/project/T2D/ATAC-seq/NextSeq/0raw/merge/$(basename "$filename" R1_001.fastq.gz)R2_001.fastq.gz  /restricted/projectnb/chengcancer/Simon/project/T2D/ATAC-seq/NextSeq/0raw/merge/$(basename "$filename" R1_001.fastq.gz)1P.fq.gz  /restricted/projectnb/chengcancer/Simon/project/T2D/ATAC-seq/NextSeq/0raw/merge/$(basename "$filename" R1_001.fastq.gz)1U.fq.gz  /restricted/projectnb/chengcancer/Simon/project/T2D/ATAC-seq/NextSeq/0raw/merge/$(basename "$filename" R1_001.fastq.gz)2P.fq.gz   /restricted/projectnb/chengcancer/Simon/project/T2D/ATAC-seq/NextSeq/0raw/merge/$(basename "$filename" R1_001.fastq.gz)2U.fq.gz   HEADCROP:12
+java -jar $SCC_TRIMMOMATIC_BIN/trimmomatic.jar PE  "$filename"  ./$(basename "$filename" R1_001.fastq.gz)R2_001.fastq.gz  ./$(basename "$filename" R1_001.fastq.gz)1P.fq.gz  ./$(basename "$filename" R1_001.fastq.gz)1U.fq.gz  ./$(basename "$filename" R1_001.fastq.gz)2P.fq.gz  ./$(basename "$filename" R1_001.fastq.gz)2U.fq.gz   HEADCROP:12
 
 
 done
